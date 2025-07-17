e606955f8fd17e3228779e8a01deaea28158f022
Akhlas789 <akhlasuddin54321@gmail.com>
2025-07-17 17:22:24 +0600

    featured product done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-fade":
+          "linear-gradient(, rgb(244, 244, 244), rgba(244, 244, 244, 0) 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
